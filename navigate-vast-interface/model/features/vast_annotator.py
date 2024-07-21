d47570a9d5f735e15290b453a8a8ba9c18c7e957
Conor McFadden <conorhughmcfadden@gmail.com>
2024-07-21 14:54:30 -0500

    This was just for demo talking with Annie...

--- navigate-vast-interface/model/features/vast_annotator.py
+++ navigate-vast-interface/model/features/vast_annotator.py
@@ -31,7 +31,6 @@ class VastAnnotator:
     def in_func_signal(self):
         """set devices before snaping an image"""
         pass
-        self.model.event_queue.put(("do_something", [data]))
 
     def end_func_signal(self):
         """decide if this feature ends after snaping an image"""
