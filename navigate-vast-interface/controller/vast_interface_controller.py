7cfbd9a99f51d513dca6d945065b6b8d0433a280
conorhughmcfadden <conorhughmcfadden@gmail.com>
2024-08-01 18:14:37 -0500

    View Updates - Connor
    Added controller code for basics of GUI interface, started playing with resizing/fluorescent channel flipping on image display

--- navigate-vast-interface/controller/vast_interface_controller.py
+++ navigate-vast-interface/controller/vast_interface_controller.py
@@ -1,17 +1,49 @@
+# Standard library imports
+import tkinter as tk
+from tkinter import filedialog
+
+# Third party imports
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+# Local application imports
+from navigate.controller.sub_controllers.gui import GUIController
+
 class VastInterfaceController:
-    
     def __init__(self, view, parent_controller=None):
         self.view = view
         self.parent_controller = parent_controller
         
         self.variables = self.view.get_variables()
+        self.widgets = self.view.get_widgets()
         self.buttons = self.view.buttons
 
         # #################################
         # ##### Example Widget Events #####
         # #################################
-        self.buttons["move"].configure(command=self.move)
+        self.buttons["vast_storage_dir"].configure(command=self.update_vast_imagefolder)
 
+    def update_vast_imagefolder(self, *args):
+        """Update autostore path for the VAST
+
+
+        Parameters
+        ----------
+        *args
+            Not used
+
+
+        Examples
+        --------
+        self.update_vast_imagefolder()
+        """
+        print(self.widgets["VASTsavedir"].get())
+        vast_storage_dir = tk.StringVar()
+        vast_storage_dir = filedialog.askdirectory(initialdir=self.widgets["VASTsavedir"].get())
+        #self.vast_operator_vals["vast_image_folder"].set(vast_storage_dir)
+        self.widgets["VASTsavedir"].set(vast_storage_dir)
+        print(self.widgets["VASTsavedir"].get())
+    
     def move(self, *args):
         """Example function to move the plugin device
         """
@@ -19,5 +51,3 @@ class VastInterfaceController:
         print("*** Move button is clicked!")
         self.parent_controller.execute("move_plugin_device", self.variables["plugin_name"].get())
 
-
-    
--- navigate-vast-interface/view/vast_interface_frame.py
+++ navigate-vast-interface/view/vast_interface_frame.py
@@ -1,7 +1,7 @@
+
 # Standard Imports
 import tkinter as tk
 from tkinter import ttk
-from tkinter import filedialog
 
 #Third-party Imports
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -14,13 +14,13 @@ from navigate.view.custom_widgets.LabelInputWidgetFactory import LabelInput
 
 
 class VastInterfaceFrame(ttk.Frame):
-    """VAST Operator Frame
+    """Plugin Frame: Just an example
 
-    This frame contains the widgets for operating the VAST Fluoroimager and LP Sampler.
+    This frame contains the widgets for the plugin.
     """
 
     def __init__(self, root, *args, **kwargs):
-        """Initilization of the Confocal Projection Frame
+        """Initilization of the  Frame
 
         Parameters
         ----------
@@ -31,7 +31,6 @@ class VastInterfaceFrame(ttk.Frame):
         **kwargs
             Arbitrary keyword arguments.
         """
-        # Init Frame
         ttk.Frame.__init__(self, root, *args, **kwargs)
 
         # Formatting
@@ -42,85 +41,52 @@ class VastInterfaceFrame(ttk.Frame):
         #: dict: Dictionary of the widgets in the frame
         self.inputs = {}
         self.buttons = {}
-        self.variables = []
+        self.variables = {}
 
-        # Frames for widgets
-        #: tkinter.Frame: The frame that holds the position settings
-        self.interop_frame = ttk.LabelFrame(self, text="Interop Frame")
-        #: tkinter.Frame: holds pre- and post-analysis images
-        self.image_frame = ttk.LabelFrame(self, text = "Image Frame")
-        #: frame that displays extraneous information
-        self.data_frame = ttk.LabelFrame(self, text = "Data Frame")
-        self.canvas_frame = tk.Frame(self, height=256, width=512)
-        self.DorsalImage = tk.Canvas(self.canvas_frame,
-        height = 200,
-        width = 2048,
-        bd = 10,
-        highlightthickness = 10,
-        relief = "ridge")
+        # #################################
+        # ######## Example Widgets ########
+        # ##### add your widgets here #####
+        # #################################
+        label = ttk.Label(self, text="Plugin Name:")
+        label.grid(row=0, column=0, sticky=tk.NW)
+
+        self.variables["plugin_name"] = tk.StringVar(self)
+        self.inputs["plugin_name"] = ttk.Entry(
+            self, textvariable=self.variables["plugin_name"]
+        )
+        self.inputs["plugin_name"].grid(
+            row=0, column=1, sticky="N", padx=5, pady=(0, 5)
+        )
+
+        self.buttons["move"] = ttk.Button(self, text="MOVE1")
+        self.buttons["move"].grid(row=1, column=1, sticky="N", padx=6)
+        
+        
+        self.inputs["VASTsavedir"] = LabelInput(
+            self,
+            input_class=ttk.Entry,
+            input_var=tk.StringVar()
+        )
+
+        self.DorsalImage = tk.Canvas(self,
+            height = 200,
+            width = 2048,
+            bd = 10,
+            highlightthickness = 10,
+            relief = "ridge")
         self.DorsalImage.create_oval(-50,50,-50,50)
         self.DorsalImage.place(x = 0, y = 0)
-        # self.LateralImage = tk.Canvas(self.canvas_frame,
-        # height = 1100,
-        # width = 2200,
-        # bd = 10,
-        # highlightthickness = 0,
-        # relief = "ridge")
-        # self.LateralImage.place(x = 0, y = 0)
 
-        #  #: int: The width of the canvas.
-        # #: int: The height of the canvas.
-        # self.canvas_width, self.canvas_height = 512, 512
-
-        # #: tk.Canvas: The canvas that will hold the camera image.
-        # self.canvas = tk.Canvas(
-        #     width=self.canvas_width, height=self.canvas_height
-        # )
-        # self.canvas.grid(row=0, column=0, sticky=tk.NSEW, padx=5, pady=5)
-
-        # #: matplotlib.figure.Figure: The figure that will hold the camera image.
-        # self.matplotlib_figure = Figure(figsize=[6, 6], tight_layout=True)
-
-        # #: matplotlib.backends.backend_tkagg.FigureCanvasTkAgg: The canvas that will
-        # # hold the camera image.
-        # self.matplotlib_canvas = FigureCanvasTkAgg(self.matplotlib_figure, self.canvas)
-
-
-
-
-        # Grid Each Holder Frame
-        self.interop_frame.grid(row=0, column=0, sticky=tk.NSEW)
-        self.image_frame.grid(row=1, column=0, sticky=tk.NSEW)
-        self.data_frame.grid(row=0, column=1, sticky=tk.NSEW)
-        self.canvas_frame.grid(row=1,column=1,sticky=tk.NSEW)
         
         #Image path selection
-        # self.variables["storage_dir"] = ""
         self.buttons["vast_storage_dir"] = HoverButton(
-            self.interop_frame, text="Select Image Save Folder "
+            self, text="Select Image Save Folder "
         )
         self.buttons["vast_storage_dir"].grid(row=3, column=1, sticky="N", pady=2, padx=(6, 0))
         #Button for Initialization   
         self.buttons["move_VAST"] = HoverButton(
-            self.interop_frame, text="Initialize LPS+VAST"
+            self, text="Initialize LPS+VAST"
         )
-        self.buttons["move_VAST"].grid(row=4, column=1, sticky="N", pady=2, padx=(6, 0))
-        
-        self.VASTsavedir_label = ttk.Label(self.data_frame, text="VAST Imaging Directory")
-        self.VASTsavedir_label.grid(row=0, column=0, sticky="S")
-        self.inputs["VASTsavedir"] = LabelInput(
-            parent=self.data_frame,
-            input_class=ttk.Entry,
-            input_var=tk.StringVar()
-        )
-        self.inputs["VASTsavedir"].grid(row=0, column=1, sticky="N", padx=6)
-        self.inputs["VASTsavedir"].label.grid(sticky="N")
-
-
-        self.buttons["DsplayFluorChannel"] = tk.Radiobutton(self.image_frame)
-
-        
-
     # Getters
     def get_variables(self):
         """Returns a dictionary of the variables for the widgets in this frame.
@@ -132,10 +98,7 @@ class VastInterfaceFrame(ttk.Frame):
         variables : dict
             Dictionary of the variables for the widgets in this frame.
         """
-        variables = {}
-        for key, widget in self.inputs.items():
-            variables[key] = widget.get_variable()
-        return variables
+        return self.variables
 
     def get_widgets(self):
         """Returns a dictionary of the widgets in this frame.
@@ -148,3 +111,147 @@ class VastInterfaceFrame(ttk.Frame):
             Dictionary of the widgets in this frame.
         """
         return self.inputs
+
+
+# # class VastInterfaceFrame(ttk.Frame):
+# #     """VAST Operator Frame
+
+# #     This frame contains the widgets for operating the VAST Fluoroimager and LP Sampler.
+# #     """
+
+# #     def __init__(self, root, *args, **kwargs):
+# #         """Initilization of the Vast Operator Frame
+
+# #         Parameters
+# #         ----------
+# #         root : tkinter.ttk.Frame
+# #             The frame that this frame will be placed in.
+# #         *args
+# #             Variable length argument list.
+# #         **kwargs
+# #             Arbitrary keyword arguments.
+# #         """
+# #         # Init Frame
+# #         ttk.Frame.__init__(self, root, *args, **kwargs)
+
+# #         self.inputs = {}
+# #         self.buttons = {}
+# #         self.variables = {}
+
+# #         label = ttk.Label(self, text="Plugin Name:")
+# #         label.grid(row=0, column=0, sticky=tk.NW)
+
+# #          # #################################
+# #         # ######## Example Widgets ########
+# #         # ##### add your widgets here #####
+# #         # ###############################
+
+
+
+# #         # Formatting
+# #         # tk.Grid.columnconfigure(self, "all", weight=1)
+# #         # tk.Grid.rowconfigure(self, "all", weight=1)
+
+# #         # # Dictionary for widgets and buttons
+# #         # #: dict: Dictionary of the widgets in the frame
+# #         # # Frames for widgets
+# #         # #: tkinter.Frame: The frame that holds the position settings
+# #         # self.interop_frame = ttk.LabelFrame(self, text="Interop Frame")
+# #         # #: tkinter.Frame: holds pre- and post-analysis images
+# #         # self.image_frame = ttk.LabelFrame(self, text = "Image Frame")
+# #         # #: frame that displays extraneous information
+# #         # self.data_frame = ttk.LabelFrame(self, text = "Data Frame")
+# #         # self.canvas_frame = tk.Frame(self, height=256, width=512)
+# #         # self.DorsalImage = tk.Canvas(self.canvas_frame,
+# #         # height = 200,
+# #         # width = 2048,
+# #         # bd = 10,
+# #         # highlightthickness = 10,
+# #         # relief = "ridge")
+# #         # self.DorsalImage.create_oval(-50,50,-50,50)
+# #         # self.DorsalImage.place(x = 0, y = 0)
+# #         # # self.LateralImage = tk.Canvas(self.canvas_frame,
+# #         # # height = 1100,
+# #         # # width = 2200,
+# #         # # bd = 10,
+# #         # # highlightthickness = 0,
+# #         # # relief = "ridge")
+# #         # # self.LateralImage.place(x = 0, y = 0)
+
+# #         # #  #: int: The width of the canvas.
+# #         # # #: int: The height of the canvas.
+# #         # # self.canvas_width, self.canvas_height = 512, 512
+
+# #         # # #: tk.Canvas: The canvas that will hold the camera image.
+# #         # # self.canvas = tk.Canvas(
+# #         # #     width=self.canvas_width, height=self.canvas_height
+# #         # # )
+# #         # # self.canvas.grid(row=0, column=0, sticky=tk.NSEW, padx=5, pady=5)
+
+# #         # # #: matplotlib.figure.Figure: The figure that will hold the camera image.
+# #         # # self.matplotlib_figure = Figure(figsize=[6, 6], tight_layout=True)
+
+# #         # # #: matplotlib.backends.backend_tkagg.FigureCanvasTkAgg: The canvas that will
+# #         # # # hold the camera image.
+# #         # # self.matplotlib_canvas = FigureCanvasTkAgg(self.matplotlib_figure, self.canvas)
+
+
+
+
+# #         # # Grid Each Holder Frame
+# #         # self.interop_frame.grid(row=0, column=0, sticky=tk.NSEW)
+# #         # self.image_frame.grid(row=1, column=0, sticky=tk.NSEW)
+# #         # self.data_frame.grid(row=0, column=1, sticky=tk.NSEW)
+# #         # self.canvas_frame.grid(row=1,column=1,sticky=tk.NSEW)
+        
+# #         # #Image path selection
+# #         # # self.variables["storage_dir"] = ""
+# #         # self.buttons["vast_storage_dir"] = HoverButton(
+# #         #     self.interop_frame, text="Select Image Save Folder "
+# #         # )
+# #         # self.buttons["vast_storage_dir"].grid(row=3, column=1, sticky="N", pady=2, padx=(6, 0))
+# #         # #Button for Initialization   
+# #         # self.buttons["move_VAST"] = HoverButton(
+# #         #     self.interop_frame, text="Initialize LPS+VAST"
+# #         # )
+# #         # self.buttons["move_VAST"].grid(row=4, column=1, sticky="N", pady=2, padx=(6, 0))
+        
+# #         # self.VASTsavedir_label = ttk.Label(self.data_frame, text="VAST Imaging Directory")
+# #         # self.VASTsavedir_label.grid(row=0, column=0, sticky="S")
+# #         # self.inputs["VASTsavedir"] = LabelInput(
+# #         #     parent=self.data_frame,
+# #         #     input_class=ttk.Entry,
+# #         #     input_var=tk.StringVar()
+# #         # )
+# #         # self.inputs["VASTsavedir"].grid(row=0, column=1, sticky="N", padx=6)
+# #         # self.inputs["VASTsavedir"].label.grid(sticky="N")
+
+
+# #         # self.buttons["DsplayFluorChannel"] = tk.Radiobutton(self.image_frame)
+
+        
+
+# #     # Getters
+# #     def get_variables(self):
+# #         """Returns a dictionary of the variables for the widgets in this frame.
+
+# #         The key is the widget name, value is the variable associated.
+
+# #         Returns
+# #         -------
+# #         variables : dict
+# #             Dictionary of the variables for the widgets in this frame.
+# #         """
+# #         return self.variables
+
+# #     def get_widgets(self):
+# #         """Returns a dictionary of the widgets in this frame.
+
+# #         The key is the widget name, value is the LabelInput class that has all the data.
+
+# #         Returns
+# #         -------
+# #         self.inputs : dict
+# #             Dictionary of the widgets in this frame.
+# #         """
+# #         return self.inputs
