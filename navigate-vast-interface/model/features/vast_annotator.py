21cc77ad9353534e8ab77010808b8cb08ba06319
conorhughmcfadden <conorhughmcfadden@gmail.com>
2024-09-18 13:02:50 -0500

    Update vast_annotator.py

--- navigate-vast-interface/model/features/vast_annotator.py
+++ navigate-vast-interface/model/features/vast_annotator.py
@@ -30,7 +30,6 @@ class VastAnnotator:
 
     def in_func_signal(self):
         """set devices before snaping an image"""
-        print("VastAnnotator --> In func signal!!!")
 
     def end_func_signal(self):
         """decide if this feature ends after snaping an image"""
